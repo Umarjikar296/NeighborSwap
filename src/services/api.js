6e2d8f962f6bb78df14febd0e46af00b98e13271
Ashwin_Umarjikar <umarjikarashwin@gmail.com>
2025-09-28 17:15:51 +0200

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 // src/services/api.js
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5001/api';
+const API_BASE_URL = 'https://neighborswap-backend.onrender.com"';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
